7b81ace3922fef8e33559b6864a37a3a1298ca9f
ForrestMarkX <forrestmarkx@outlook.com>
2015-02-02 20:51:14 -0600

    Removed some unneeded files

--- bmtzombiesurvival/lua/autorun/server/sv_flashlight.lua
+++
@@ -1,7 +0,0 @@
-hook.Add( "OnPlayerChangedTeam", "TPF_ZombieSurvivalAddon", function( ply, oldTeam, newTeam ) 
-	if newteam == TEAM_UNDEAD then
-		pl.m_bTPFDisabled = true
-	elseif newteam == TEAM_HUMAN then
-		pl.m_bTPFDisabled = false
-	end
-end )
